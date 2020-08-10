83eef779c4c1ea44768af9d004c29fb68f019ff4
hdmcspadden <hdmcspadden@gmail.com>
2020-08-10 11:58:02 -0400

    added random number gens and histogram

--- RModule3.R
+++ RModule3.R
@@ -1,3 +1,6 @@
+# author: Diana
+# date: 8/10/2020
+
 # determine working directory
 getwd()
 
@@ -25,4 +28,25 @@ print('hello world')
 mean(20:40)
 #> 30
 
+# create a random number plot
+# load a random num gen and make some random numbers and save them
+?rnorm
+rnums <- rnorm(10)
+print(rnums)
+
+# use a different random package
+install.packages("random")
+library("random")
+
+?random
+rSeq <- randomSequence(min=1, max=20, col=1, check=TRUE)
+print(rSeq)
+
+# create a "random" vector
+rVector <- c(1,23,45,62,3,57,98,2,43)
+
+
+# make a histogram
+hist(rSeq)
+hist(rVector)
 
