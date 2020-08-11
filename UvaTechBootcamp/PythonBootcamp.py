58252f7664e9959d6400cbd5856c4a52363e2a2d
hdmcspadden <hdmcspadden@gmail.com>
2020-08-11 08:36:49 -0400

    added RStopwatch and Python exercises in additional UVaTechBootcamp folder

---
+++ UvaTechBootcamp/PythonBootcamp.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 10 15:59:59 2020
+
+@author: Diana McSpadden
+"""
+
+import numpy as np
+import random
+
+randnum = random.random()
+print(randnum)
+
+print("hello world")
+
